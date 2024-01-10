6db547265db5366eb097129ba83771d9dd2280de
aamiraghaa <137687371+aamiraghaa@users.noreply.github.com>
2024-01-10 16:03:15 +0400

    Update script.js

--- script.js
+++ script.js
@@ -1,7 +1,19 @@
 document.addEventListener('DOMContentLoaded', function () {
+    var timeSelection = document.getElementById('timeSelection');
     var amountSelection = document.getElementById('amountSelection');
     var amountOutput = document.getElementById('amountOutput');
 
+    // Set default time value to the current hour
+    var currentHour = new Date().getHours();
+    timeSelection.value = currentHour;
+
+    // Update the output for the time slider
+    timeSelection.addEventListener('input', function () {
+        var selectedTime = parseInt(timeSelection.value);
+        timeSelection.value = selectedTime;
+    });
+
+    // Update the output for the amount slider
     amountSelection.addEventListener('input', function () {
         amountOutput.textContent = amountSelection.value + ' ml';
     });
